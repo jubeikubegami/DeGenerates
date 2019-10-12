4152cfdd9ca5affa932f1bd9a371c9ec5ab48a2e
David Lacy <david.lacy@itg.gatech.edu>
2019-10-11 22:00:16 -0400

    change to Yang image function now works

--- p5.play-master/sketch.js
+++ p5.play-master/sketch.js
@@ -13,6 +13,7 @@ var chaChing;
 var deathScream;
 var jump;
 var music;
+var button;
 // let TrumpPct = [];
 var bidenPctSum = 0;
 var bidenPctAvg;
@@ -111,7 +112,11 @@ function setup() {
   updateSprites(false);
 
   camera.position.y = height/2;
+  //changing character to Andrew Yang
+button = createButton("yang");
+button.mousePressed(changeYang);
 }
+
 
 function draw() {
 
@@ -156,6 +161,7 @@ function draw() {
       if(pipes[i].position.x < bird.position.x-width/2)
         pipes[i].remove();
 
+
   }
 
   camera.position.x = bird.position.x + width/4;
@@ -198,4 +204,8 @@ function mousePressed() {
   bird.velocity.y = FLAP;
   //play jump sound when click
   jump.play();
+}
+function changeYang(){
+birdImg = loadImage('assets/yang.png');
+bird.addImage(birdImg);
 }
